2f7a90aa1a6244291a4430c718dca3c1af635d19
Omara493 <omemevlutoglu@stu.okan.edu.tr>
2023-05-13 12:14:53 +0300

    first commit

---
+++ lib/Pages/home.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home")),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            child: Text("Log out")),
+      ),
+    );
+  }
+}
