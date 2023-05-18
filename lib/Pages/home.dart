be02eebfbce10dbd1f208610357f1d0e17f9955b
Omara493 <omemevlutoglu@stu.okan.edu.tr>
2023-05-19 02:59:27 +0300

    modified:   lib/Pages/home.dart

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_build_context_synchronously
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously, prefer_final_fields
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -13,84 +13,112 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    // Add your desired page widgets for each tab
+    Text(' Home'),
+    Text('Settings'),
+    Text('Profile'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home")),
       drawer: SafeArea(
-        child: SizedBox(
-          height: 350,
-          child: Drawer(
-            child: Padding(
-              padding: EdgeInsets.all(
-                  16.0), // Add desired padding around the content
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Search', // Placeholder text for the search bar
-                      prefixIcon: Icon(Icons.search), // Icon for the search bar
+        child: Drawer(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search', // Placeholder text for the search bar
+                    prefixIcon: Icon(Icons.search), // Icon for the search bar
+                  ),
+                  onChanged: (value) {},
+                ),
+                SizedBox(height: 16.0),
+                Column(
+                  children: [
+                    ListTile(
+                      title: Text("Home"),
+                      leading: Icon(Icons.home),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Home(),
+                          ),
+                        );
+                      },
                     ),
-                    onChanged: (value) {
-                      // Handle search functionality
-                    },
-                  ),
-                  SizedBox(
-                      height:
-                          16.0), // Add spacing between search bar and list tiles
-                  Column(
-                    children: [
-                      ListTile(
-                        title: Text("Home"),
-                        leading: Icon(Icons.home),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Home(),
-                            ),
-                          );
-                        },
-                      ),
-                      ListTile(
-                        title: Text("My products"),
-                        leading: Icon(Icons.add_shopping_cart),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => LoginScreen(),
-                            ),
-                          );
-                        },
-                      ),
-                      ListTile(
-                        title: Text("About"),
-                        leading: Icon(Icons.help_center),
-                        onTap: () {},
-                      ),
-                      ListTile(
-                        title: Text("Logout"),
-                        leading: Icon(Icons.exit_to_app),
-                        onTap: () async {
-                          Navigator.pop(context);
-                          await FirebaseAuth.instance.signOut();
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => LoginScreen(),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    ListTile(
+                      title: Text("My products"),
+                      leading: Icon(Icons.add_shopping_cart),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text("About"),
+                      leading: Icon(Icons.help_center),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      title: Text("Logout"),
+                      leading: Icon(Icons.exit_to_app),
+                      onTap: () async {
+                        Navigator.pop(context);
+                        await FirebaseAuth.instance.signOut();
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
